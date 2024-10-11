9fc8d306ca908e8d95c0273ccf2b78784bf6518c
Nick Much <500160291@fvtc.edu>
2024-10-11 05:19:30 -0500

    Initial commit

---
+++ src/main/java/org/outliertech/outliereconomy/OutlierEconomy.java
@@ -0,0 +1,16 @@
+package org.outliertech.outliereconomy;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OutlierEconomy implements ModInitializer {
+	public static final String MOD_ID = "outlier-economy";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/org/outliertech/outliereconomy/OutlierEconomyClient.java
@@ -0,0 +1,10 @@
+package org.outliertech.outliereconomy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OutlierEconomyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
